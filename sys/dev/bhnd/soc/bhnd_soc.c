7371aff66ef42e2e9d76af843b40ebf2e7be7b00
Michael Zhilin <zhilin@netcracker.com>
2016-02-25 20:22:59 +0300

    (wip) add FIXED window to support SoC MMIO

--- sys/dev/bhnd/soc/bhnd_soc.c
+++ sys/dev/bhnd/soc/bhnd_soc.c
@@ -5,25 +5,61 @@
  *      Author: mizhka
  */
 
-#include<sys/param.h>
-#include<sys/kernel.h>
-#include<sys/bus.h>
-#include<sys/module.h>
+#include <sys/param.h>
+#include <sys/kernel.h>
+#include <sys/bus.h>
+#include <sys/module.h>
 
-#include<sys/errno.h>
+#include <sys/errno.h>
 
-#include<machine/resource.h>
-#include<dev/bhnd/bhndb/bhndb.h>
-#include<dev/bhnd/bhnd_types.h>
+#include <machine/resource.h>
 
-struct bhnd_soc_softc {
-	device_t dev;
-	device_t bridge;
-};
+#include <dev/bhnd/bhnd_types.h>
 
-static const struct bhndb_hwcfg * bhnd_soc_get_generic_hwcfg(device_t dev, device_t child);
-//static struct bhndb_hw * bhnd_soc_get_bhndb_hwtable(device_t dev, device_t child);
-//static bool bhnd_soc_is_core_disabled(device_t dev, device_t child, struct bhnd_core_info *core);
+#include <dev/bhnd/bhndb/bhndb.h>
+#include <dev/bhnd/soc/bhnd_soc.h>
+
+/*
+ * **************************** VARIABLES ****************************
+ */
+
+extern const struct bhndb_hw bhndb_pci_generic_hw_table[];
+extern const struct bhndb_hwcfg bhnd_soc_bcma_generic_hwcfg;
+
+int attachment_mode = SOC_TO_BRIDGE;
+
+/*
+ * **************************** PROTOTYPES ****************************
+ */
+//internal methods
+static int 	bhnd_soc_attach_bridge(device_t dev, struct bhnd_soc_softc* sc);
+static int 	bhnd_soc_attach_bus(device_t dev, struct bhnd_soc_softc* sc);
+//device methods
+static int 	bhnd_soc_probe(device_t dev);
+static int 	bhnd_soc_attach(device_t dev);
+//bhndb methods
+static const struct bhndb_hwcfg * 	bhnd_soc_get_generic_hwcfg(device_t dev, device_t child);
+static const struct bhndb_hw * 			bhnd_soc_get_bhndb_hwtable(device_t dev, device_t child);
+static bool bhnd_soc_is_core_disabled(device_t dev, device_t child, struct bhnd_core_info *core);
+//TODO:
+
+
+/*
+ * **************************** IMPLEMENTATION ****************************
+ */
+
+static int bhnd_soc_attach_bridge(device_t dev, struct bhnd_soc_softc* sc){
+	int err = bhndb_attach_bridge(dev, &sc->bridge, 0);
+	if(err != 0){
+		device_printf(dev,"can't attach bridge: %d\n", err);
+		return err;
+	}
+	return 0;
+}
+
+static int bhnd_soc_attach_bus(device_t dev, struct bhnd_soc_softc* sc){
+	return bhndb_attach_by_class(dev, &(sc->bus), -1, bhnd_devclass);
+}
 
 static int bhnd_soc_probe(device_t dev){
 	device_set_desc(dev, "Broadcom SOC for HND");
@@ -35,58 +71,35 @@ static int bhnd_soc_attach(device_t dev){
 	sc->dev = dev;
 	BUS_PRINT_CHILD(device_get_parent(dev), dev);
 
-	int err = bhndb_attach_bridge(dev, &sc->bridge, 0);
-	if(err != 0){
-		device_printf(dev,"can't attach bridge: %d\n", err);
-		return err;
-	}
-
-	return 0;
+	return (attachment_mode == SOC_TO_BRIDGE) ?
+		bhnd_soc_attach_bridge(dev,sc) :
+		bhnd_soc_attach_bus(dev,sc);
 }
-
-//us
-#define BHND_SOC_CCREGS_OFFSET	0x18000000
-#define	BHND_SOC_CCREGS_SIZE	0x1000
-
-#define BHND_SOC_EROMREGS_OFFSET 0x1810e000
-#define BHND_SOC_EROMREGS_SIZE	 0x1000
-
-const struct bhndb_hwcfg bhnd_soc_bcma_generic_hwcfg = {
-	.resource_specs		= (const struct resource_spec[]) {
-		{ SYS_RES_MEMORY,	0,	RF_ACTIVE },
-		{ -1,			0,		0 }
-	},
-
-	.register_windows	= (const struct bhndb_regwin[]) {
-		/* 0x18000000: chipc core registers */
-		{
-			.win_type	= BHNDB_REGWIN_T_CORE,
-			.win_offset	= BHND_SOC_CCREGS_OFFSET,
-			.win_size	= BHND_SOC_CCREGS_SIZE,
-			.res		= {
-					.type = SYS_RES_MEMORY,
-					.rid  = 0
-			},
-			.win_spec.core 		= {
-					.class	= BHND_DEVCLASS_CC,
-					.unit	= 0,
-					.port	= 0,
-					.region	= 0,
-					.port_type = BHND_PORT_DEVICE
-			}
-		},
-		BHNDB_REGWIN_TABLE_END
-	},
-};
 
 static const struct bhndb_hwcfg * bhnd_soc_get_generic_hwcfg(device_t dev, device_t child){
 	return &bhnd_soc_bcma_generic_hwcfg;
 }
 
+static const struct bhndb_hw * 		  bhnd_soc_get_bhndb_hwtable(device_t dev, device_t child){
+	return bhndb_pci_generic_hw_table;
+}
+
+static bool bhnd_soc_is_core_disabled(device_t dev, device_t child, struct bhnd_core_info *core){
+	return false;
+}
+
+/*
+ * **************************** DRIVER METADATA ****************************
+ */
+
 static device_method_t bhnd_soc_methods[] = {
+		//device interface
 		DEVMETHOD(device_probe,		bhnd_soc_probe),
 		DEVMETHOD(device_attach,	bhnd_soc_attach),
-		DEVMETHOD(bhndb_bus_get_generic_hwcfg,bhnd_soc_get_generic_hwcfg	),
+		//bhndb_bus interface
+		DEVMETHOD(bhndb_bus_get_generic_hwcfg , bhnd_soc_get_generic_hwcfg),
+		DEVMETHOD(bhndb_bus_get_hardware_table, bhnd_soc_get_bhndb_hwtable),
+		DEVMETHOD(bhndb_bus_is_core_disabled,	bhnd_soc_is_core_disabled),
 		DEVMETHOD_END
 };
 
