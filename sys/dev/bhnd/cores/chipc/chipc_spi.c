67dac9c1fed74d1f7606a3a39de0d63b4c12f73d
Michael Zhilin <zhilin@netcracker.com>
2016-06-07 15:40:23 +0300

    [chipc/spi] increase logging for busy message

--- sys/dev/bhnd/cores/chipc/chipc_spi.c
+++ sys/dev/bhnd/cores/chipc/chipc_spi.c
@@ -205,7 +205,9 @@ chipc_spi_wait(struct chipc_spi_softc *sc)
 	if (i > 0)
 		return (0);
 
-	BHND_DEBUG_DEV(sc->dev, "busy");
+	BHND_WARN_DEV(sc->dev, "busy: CTL=0x%x DATA=0x%x",
+			SPI_READ(sc, CHIPC_SPI_FLASHCTL),
+			SPI_READ(sc, CHIPC_SPI_FLASHDATA));
 	return (-1);
 }
 
