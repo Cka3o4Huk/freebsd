278e0d13508bbfd0ab0b61fcb82b751eb03303ee
Michael Zhilin <zhilin@netcracker.com>
2016-07-25 14:06:09 +0300

    [etherswitch] add etherswitch support for Broadcom SOHO routers: b5357/5358 works, DDB, VLAN(4) device into kernconf

---
+++ sys/dev/etherswitch/b53/b53_hal.h
@@ -0,0 +1,41 @@
+/*-
+ * Copyright (c) 2011 Aleksandr Rybalko.
+ * Copyright (c) 2016 Michael Zhilin <mizhka@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef _B53_HAL_H_
+#define _B53_HAL_H_
+
+#include "b53_var.h"
+
+extern struct b53_hal b5325_hal;	/* BCM5325 */
+extern struct b53_hal b5358_hal;	/* BCM5358 */
+extern struct b53_hal b53115_hal;	/* BCM5395/53115/53118; 5397?/5398? */
+
+void		b53hal_init(struct b53_softc *sc);
+
+#endif /* _B53_HAL_H_ */
