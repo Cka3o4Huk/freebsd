12d03791714ef35de18d97285742a44dc91b9ab2
Michael Zhilin <zhilin@netcracker.com>
2016-09-03 22:43:37 +0300

    [BCM/MIPS74k] IRQ resource discovery

--- sys/mips/broadcom/bcm_mips74k.c
+++ sys/mips/broadcom/bcm_mips74k.c
@@ -35,14 +35,22 @@ __FBSDID("$FreeBSD$");
 #include <sys/kernel.h>
 #include <sys/bus.h>
 #include <sys/module.h>
+#include <sys/systm.h>
+#include <sys/errno.h>
+#include <sys/rman.h>
+#include <sys/stddef.h>
 
 #include <machine/bus.h>
-#include <sys/rman.h>
 #include <machine/resource.h>
 
 #include <dev/bhnd/bhnd.h>
+#include <dev/bhnd/bhndvar.h>
+#include <dev/bhnd/bhnd_ids.h>
+
+#include <dev/bhnd/bcma/bcma_dmp.h>
 
 #include "bcm_mips74kreg.h"
+
 
 /*
  * Broadcom MIPS74K Core
@@ -50,6 +58,14 @@ __FBSDID("$FreeBSD$");
  * These cores are only found on bcma(4) chipsets, allowing
  * us to assume the availability of bcma interrupt registers.
  */
+
+#define	BHND_MIPS74K_IRQN	5
+#define	BHND_MIPS74K_IRQBASE	2
+
+static const struct resource_spec mipscore_rspec[MIPSCORE_MAX_RSPEC] = {
+	{ SYS_RES_MEMORY,	0,	RF_ACTIVE },
+	{ -1, -1, 0 }
+};
 
 static const struct bhnd_device bcm_mips74k_devs[] = {
 	BHND_DEVICE(MIPS, MIPS74K, NULL, NULL, BHND_DF_SOC),
@@ -80,6 +96,14 @@ static int
 bcm_mips74k_attach(device_t dev)
 {
 	struct bcm_mips74k_softc *sc;
+	struct resource 	*res;
+	device_t		*children;
+	uint32_t		 tmp;
+	uint32_t 		 intmasks[BHND_MIPS74K_IRQN];
+	uint16_t		 devid;
+	int			 error;
+	int 			 offset, count;
+	int			 i, j;
 
 	sc = device_get_softc(dev);
 	sc->dev = dev;
@@ -94,6 +118,52 @@ bcm_mips74k_attach(device_t dev)
 	/* Route MIPS timer to IRQ5 */
 	bus_write_4(sc->mem_res, BCM_MIPS74K_INTR5_SEL,
 	    (1<<BCM_MIPS74K_TIMER_IVEC));
+
+	/* Use intmask0-4 to identify mapping IRQ to cores */
+	offset = offsetof(struct mipscore_regs, intmask[0]);
+
+	/* scan IRQ masks of lines */
+	for (i = 0; i < BHND_MIPS74K_IRQN; i++) {
+		intmasks[i] = bus_read_4(res, offset);
+		offset += sizeof(uint32_t);
+	}
+
+	error = device_get_children(device_get_parent(dev), &children,
+	    &count);
+
+	if (error) {
+		BHND_ERROR_DEV(dev, "can't get list of BHND devices: %d",
+		    error);
+		return (error);
+	}
+
+	for (i = 0; i < count; i++) {
+		tmp = bhnd_read_config(children[i],
+		    BCMA_DMP_OOBSELOUTA30, sizeof(uint32_t));
+		if (tmp == 0)
+			continue;
+
+		/* filter bits by LINE mask */
+		tmp &= 0x1f;
+		/* calculate bitmask according to line number */
+		tmp = (1 << tmp);
+
+		/* try find line in IRQ bit masks */
+		for (j = 0; j < BHND_MIPS74K_IRQN; j++) {
+			if ((intmasks[j] & tmp) == 0)
+				continue;
+
+			BHND_INFO_DEV(dev, "%s: IRQ %d as rid 0",
+			    bhnd_get_device_name(children[i]),
+			    BHND_MIPS74K_IRQBASE + j);
+
+			bus_set_resource(children[i], SYS_RES_IRQ, 0,
+			    BHND_MIPS74K_IRQBASE + j, 1);
+
+			break;
+		}
+	}
+	free(children, M_TEMP);
 
 	return (0);
 }
