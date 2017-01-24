ee2cb2a408829afc1ccf2aebe05cb79c2cce63c3
Michael Zhilin <zhilin@netcracker.com>
2017-01-24 12:54:50 +0300

    [robosw] rename b53 to robosw & add module

--- sys/dev/etherswitch/robosw/bcm53115.c
+++ sys/dev/etherswitch/robosw/bcm53115.c
@@ -31,62 +31,62 @@
 #include <sys/bus.h>
 #include <sys/errno.h>
 
-#include "b53_var.h"
-#include "b53_reg.h"
-#include "b53_hal.h"
+#include "robosw_var.h"
+#include "robosw_reg.h"
+#include "robosw_hal.h"
 
-static int	b53115hal_get_vlan_group(struct b53_softc *sc, int vlan_group,
+static int	bcm53115_get_vlan_group(struct robosw_softc *sc, int vlan_group,
 		    int *vlan_id, int *members, int *untagged, int *forward_id);
-static int	b53115hal_set_vlan_group(struct b53_softc *sc, int vlan_group,
+static int	bcm53115_set_vlan_group(struct robosw_softc *sc, int vlan_group,
 		    int vlan_id, int members, int untagged, int forward_id);
-static int 	b53115hal_reset(struct b53_softc *sc);
-static int 	b53115hal_vlan_enable(struct b53_softc *sc, int on);
+static int 	bcm53115_reset(struct robosw_softc *sc);
+static int 	bcm53115_vlan_enable(struct robosw_softc *sc, int on);
 
-struct b53_functions b53115_f = {
-	.reset = b53115hal_reset,
-	.vlan_enable = b53115hal_vlan_enable,
-	.vlan_get_vlan_group = b53115hal_get_vlan_group,
-	.vlan_set_vlan_group = b53115hal_set_vlan_group
+struct robosw_functions bcm53115_f = {
+	.api.reset = bcm53115_reset,
+	.api.vlan_enable = bcm53115_vlan_enable,
+	.api.vlan_get_vlan_group = bcm53115_get_vlan_group,
+	.api.vlan_set_vlan_group = bcm53115_set_vlan_group
 };
 
-struct b53_hal b53115_hal = {
-	.parent = &b5325_hal,
-	.own = &b53115_f
+struct robosw_hal bcm53115_hal = {
+	.parent = &bcm5325_hal,
+	.self = &bcm53115_f
 };
 
 static int
-b53115hal_reset(struct b53_softc *sc)
+bcm53115_reset(struct robosw_softc *sc)
 {
 	int		err;
 	uint32_t	reg;
 
 	/* MII port state override (page 0 register 14) */
-	err = b53chip_op(sc, PORTMII_STATUS_OVERRIDE, &reg, 0);
+	err = robosw_op(sc, PORTMII_STATUS_OVERRIDE, &reg, 0);
 
 	if (err) {
 		device_printf(sc->sc_dev, "Unable to set RvMII mode\n");
 		return (ENXIO);
 	}
 
-	b53chip_write4(sc, PORTMII_STATUS_OVERRIDE, reg |
+	robosw_write4(sc, PORTMII_STATUS_OVERRIDE, reg |
 	    PORTMII_STATUS_FORCE_1000M | PORTMII_STATUS_FORCE_LINK );
 
 	return (0);
 }
 
 static int
-b53115hal_vlan_enable(struct b53_softc *sc, int on)
+bcm53115_vlan_enable(struct robosw_softc *sc, int on)
 {
 	uint32_t	ctl0, ctl1, drop, ctl4, ctl5, sw;
 
 	drop = 0;
 
-	B53_RD(VLAN_GLOBAL_CTL0, ctl0, sc);
-	B53_RD(VLAN_GLOBAL_CTL1, ctl1, sc);
+	ROBOSW_RD(VLAN_GLOBAL_CTL0, ctl0, sc);
+	ROBOSW_RD(VLAN_GLOBAL_CTL1, ctl1, sc);
 	/* b53115-specific */
-	B53_RD(VLAN_GLOBAL_CTL4_531xx, ctl4, sc);
+	ROBOSW_RD(VLAN_GLOBAL_CTL4_531xx, ctl4, sc);
 	ctl4 &= ~VLAN_GLOBAL_CTL4_VID_MASK;
-	B53_RD(VLAN_GLOBAL_CTL5_531xx, ctl5, sc);
+	ROBOSW_RD(VLAN_GLOBAL_CTL5_531xx, ctl5, sc);
 
 	device_printf(sc->sc_dev, "ctl <=: %x/%x/%x/%x\n", ctl0, ctl1, ctl4,
 	    ctl5);
@@ -118,40 +118,40 @@ b53115hal_vlan_enable(struct b53_softc *sc, int on)
 	device_printf(sc->sc_dev, "ctl =>: %x/%x/%x/%x\n", ctl0, ctl1, ctl4,
 	    ctl5);
 
-	B53_WR(VLAN_GLOBAL_CTL0, ctl0, sc);
-	B53_WR(VLAN_GLOBAL_CTL1, ctl1, sc);
+	ROBOSW_WR(VLAN_GLOBAL_CTL0, ctl0, sc);
+	ROBOSW_WR(VLAN_GLOBAL_CTL1, ctl1, sc);
 	/* b53115-specific */
-	B53_WR(VLAN_DROP_UNTAGGED, drop, sc);
-	B53_WR(VLAN_GLOBAL_CTL4_531xx, ctl4, sc);
-	B53_WR(VLAN_GLOBAL_CTL5_531xx, ctl5, sc);
+	ROBOSW_WR(VLAN_DROP_UNTAGGED, drop, sc);
+	ROBOSW_WR(VLAN_GLOBAL_CTL4_531xx, ctl4, sc);
+	ROBOSW_WR(VLAN_GLOBAL_CTL5_531xx, ctl5, sc);
 
-	B53_RD(SWITCH_MODE, sw, sc);
+	ROBOSW_RD(SWITCH_MODE, sw, sc);
 	sw &= ~SWITCH_MODE_MANAGED;
-	B53_WR(SWITCH_MODE, sw, sc);
+	ROBOSW_WR(SWITCH_MODE, sw, sc);
 	return (0);
 }
 
 static int
-b53115hal_get_vlan_group(struct b53_softc *sc, int vlan_group,
-		    int *vlan_id, int *members, int *untagged, int *forward_id)
+bcm53115_get_vlan_group(struct robosw_softc *sc, int vlan_group,
+    int *vlan_id, int *members, int *untagged, int *forward_id)
 {
 	uint32_t		 reg;
 
 	/* Set VLAN_GROUP as input */
-	B53_WR(VLAN_TABLE_INDX_5395, vlan_group, sc);
+	ROBOSW_WR(VLAN_TABLE_INDX_5395, vlan_group, sc);
 	/* Execute READ vlan information */
 	reg = VLAN_TABLE_ACCESS_5395_RUN | VLAN_TABLE_ACCESS_5395_READ;
-	B53_WR(VLAN_TABLE_ACCESS_5395, reg, sc);
+	ROBOSW_WR(VLAN_TABLE_ACCESS_5395, reg, sc);
 	/* Read result */
-	B53_RD(VLAN_TABLE_ENTRY_5395, reg, sc);
+	ROBOSW_RD(VLAN_TABLE_ENTRY_5395, reg, sc);
 
 #if 0
 	printf("reg[%d] = 0x%x\n",vlan_group, reg);
 #endif
 
 	*vlan_id = ETHERSWITCH_VID_VALID | vlan_group; /* XXX: ??? */
-	*members = B53_UNSHIFT(reg, VLAN_RW_MEMBER_5395); /* TODO: shift MUST be 9 */
-	*untagged = B53_UNSHIFT(reg, VLAN_RW_UNTAGGED_5395);
+	*members = ROBOSW_UNSHIFT(reg, VLAN_RW_MEMBER_5395); /* TODO: shift MUST be 9 */
+	*untagged = ROBOSW_UNSHIFT(reg, VLAN_RW_UNTAGGED_5395);
 	/* TODO: forwarding */
 	*forward_id = 0; // RTL8366RB_VMCR_FID(vmcr);
 
@@ -159,23 +159,21 @@ b53115hal_get_vlan_group(struct b53_softc *sc, int vlan_group,
 }
 
 static int
-b53115hal_set_vlan_group(struct b53_softc *sc, int vlan_group,
-		    int vlan_id, int members, int untagged, int forward_id)
+bcm53115_set_vlan_group(struct robosw_softc *sc, int vlan_group,
+    int vlan_id, int members, int untagged, int forward_id)
 {
 	uint32_t		 reg;
 
 	/* TODO: check range of vlan group */
-
 	/* Set new entry */
-	reg = B53_SHIFT(untagged, VLAN_RW_UNTAGGED_5395);
-	reg |= B53_SHIFT(members, VLAN_RW_MEMBER_5395);
-	B53_WR(VLAN_TABLE_ENTRY_5395, reg, sc);
+	reg = ROBOSW_SHIFT(untagged, VLAN_RW_UNTAGGED_5395);
+	reg |= ROBOSW_SHIFT(members, VLAN_RW_MEMBER_5395);
+	ROBOSW_WR(VLAN_TABLE_ENTRY_5395, reg, sc);
 	/* Set VLAN_GROUP as input */
-	B53_WR(VLAN_TABLE_INDX_5395, vlan_group, sc);
+	ROBOSW_WR(VLAN_TABLE_INDX_5395, vlan_group, sc);
 	/* Execute READ vlan information */
 	reg = VLAN_TABLE_ACCESS_5395_RUN;
-	B53_WR(VLAN_TABLE_ACCESS_5395, reg, sc);
+	ROBOSW_WR(VLAN_TABLE_ACCESS_5395, reg, sc);
 
 	return (0);
 }
-
